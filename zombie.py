1ab471c2770ce51ea4757b3adac4a2279d424e07
laptop-msi <wlgns7922@gmail.com>
2025-11-04 12:24:15 +0900

    공에 1회 맞은 좀비는 크기가 작아짐

--- zombie.py
+++ zombie.py
@@ -30,6 +30,7 @@ class Zombie:
 
     def __init__(self):
         self.x, self.y = random.randint(1600-800, 1600), 150
+        self.w, self.h = 200, 200
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
@@ -52,9 +53,9 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.w, self.h)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.w, self.h)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
@@ -66,3 +67,6 @@ class Zombie:
                 game_world.remove_object(self)
             else:
                 self.damaged = True
+                self.w /= 2
+                self.h /= 2
+                self.y -= self.h / 2
